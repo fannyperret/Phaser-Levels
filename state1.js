3bbfad34df497c968be19090c4de701511e7470e
Fanny Perret <fannyperretlombard@gmail.com>
2018-02-28 17:07:13 +0100

    add new text :cat:

--- state1.js
+++ state1.js
@@ -1,7 +1,8 @@
 var demo = {};
-demo.state1 = function() {};
+demo.state1 = function(){};
 demo.state1.prototype = {
   preload: function() {},
   create: function() {},
-  update: function() {},
+  update: function() {}
+
 };
