c3563c2bf38e8d8510a2934752181d084bcf6d85
Fanny Perret <fannyperretlombard@gmail.com>
2018-03-13 14:40:19 +0100

    Readme and bugs, Phaser stills not worling anymore :( :cat:

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1000, 750, Phaser.AUTO);
+var game = new Phaser.Game(1000, 750, Phaser.AUTO, 'demo');
 
 game.state.add('state0', demo.state0);
 game.state.add('state1', demo.state1);
